0dd5e8ae9d9016fa96540ee0b05446749f6dd9b1
Arda Satata <ardasatata@gmail.com>
2020-12-16 03:23:50 +0800

    init tailwind

--- English_Class_Materials/baw2/weather-starter-code/src/App.js
+++ English_Class_Materials/baw2/weather-starter-code/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-200 h-screen flex items-center justify-center">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-2xl">
+          Edit <code>src/Home.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-sm font-light"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is the weather-app starter code for Build a Website II
         </a>
       </header>
     </div>
